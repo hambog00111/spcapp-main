8547e4e317fd5fc1a94642f39d2b60a11e78146b
MIS-PC_11 <chris.velasco2795@gmail.com>
2019-01-16 11:23:53 +0800

    Fragments-Gallery-HomeImageSlider

---
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.mis_pc_11.spc_app1
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        displayscreen(-1)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_BPO -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun displayscreen(id: Int){
+      val fragment= when(id){
+            R.id.nav_home -> {
+                FragmentTheCity()
+            }
+            R.id.nav_thecity -> {
+                FragmentTheCity()
+            }
+            R.id.nav_goverment ->{
+                FragmentHome()
+            }
+            R.id.nav_economy ->{
+                FragmentHome()
+            }
+            R.id.nav_sanpablo ->{
+                FragmentHome()
+            }
+            R.id.nav_tourism->{
+                FragmentHome()
+            }
+            else -> {
+                FragmentHome()
+            }
+        }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.RelativeLayout, fragment)
+            .commit()
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        displayscreen(item.itemId)
+
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
+
