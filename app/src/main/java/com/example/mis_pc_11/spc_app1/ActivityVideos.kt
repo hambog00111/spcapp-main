a99533161b146b655a20e3b7d560fd06cdfe9abb
MIS-PC_11 <chris.velasco2795@gmail.com>
2019-01-21 13:34:36 +0800

    Fragments-Gallery-HomeImageSlider

---
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/ActivityVideos.kt
@@ -0,0 +1,12 @@
+package com.example.mis_pc_11.spc_app1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityVideos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_videos)
+    }
+}
