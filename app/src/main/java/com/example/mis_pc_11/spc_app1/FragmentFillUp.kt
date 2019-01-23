0f33dee38f98382425700e0aaf0f2fa574516faf
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-01-23 12:29:39 +0800

    BPLO form linked; Preparing bplo form

--- app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentFillUp.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentFillUp.kt
@@ -1,18 +1,38 @@
 package com.example.mis_pc_11.spc_app1
 
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.*
+
 
 class FragmentFillUp : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_fill_up, null)
-
     }
 
+    abstract class SaveInfo(ctx: Context): AsyncTask<String, Void, String>() {
+
+        private var mContext: Context
+
+        init {
+            this.mContext = ctx.applicationContext
+        }
+        override fun onPreExecute(){
+
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onPostExecute(result: String?) {
+
+        }
+
+    }
 }
