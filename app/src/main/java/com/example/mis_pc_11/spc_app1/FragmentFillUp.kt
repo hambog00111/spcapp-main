4a69be6416392453ee3e765427b587499957cece
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-01-24 12:36:06 +0800

    Fill up form radio button testing; Fill up form layout changed linear to radio group

--- app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentFillUp.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentFillUp.kt
@@ -4,9 +4,13 @@ import android.app.AlertDialog
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_fill_up.view.*
 
 
@@ -21,12 +25,25 @@ class FragmentFillUp : Fragment() {
 
         val alDialog: AlertDialog = progBar.create()
         val btnClick = view.btnSend
+
+        val radGrp = view.findViewById<RadioGroup>(R.id.opt1)
+
         btnClick.setOnClickListener{
-            SaveInfo(alDialog).execute("")
+            //SaveInfo(alDialog).execute("")
+            val selRad = radGrp.checkedRadioButtonId
+            Log.d("Selected Radio Button", selRad.toString())
+            val rBtn = view.findViewById<RadioButton>(selRad)
+            Toast.makeText(context, rBtn.text, Toast.LENGTH_LONG).show()
         }
 
         return view
     }
+
+//    private fun getRadBtnVal(rg: RadioGroup, v: View){
+//        val selRad = rg.checkedRadioButtonId
+//        val rBtn = v.findViewById(selRad) as RadioButton
+//        Toast.makeText(context, rBtn.editableText, Toast.LENGTH_LONG).show()
+//    }
 
     private class SaveInfo constructor(pBar: AlertDialog): AsyncTask<String, Void, String>() {
         private val pLoading: AlertDialog = pBar
