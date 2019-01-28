76268e44a24a2ed5c32701839a4775a603732add
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-01-28 13:33:28 +0800

    Fixing unresolved conflicts

--- app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentBrowser.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentBrowser.kt
@@ -1,6 +1,5 @@
 package com.example.mis_pc_11.spc_app1
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
@@ -8,15 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.*
-import kotlinx.android.synthetic.main.fragment_gallery_layout.*
 
-class FragmentGallery : Fragment() {
+class FragmentBrowser : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_gallery_layout, null)
+        return inflater.inflate(R.layout.fragment_goverment_layout, null)
 
-
-
-        }
     }
 
+}
--- app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentFillUp.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentFillUp.kt
@@ -4,11 +4,14 @@ import android.app.AlertDialog
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_fill_up.view.*
-import java.util.*
 
 
 class FragmentFillUp : Fragment() {
@@ -22,12 +25,25 @@ class FragmentFillUp : Fragment() {
 
         val alDialog: AlertDialog = progBar.create()
         val btnClick = view.btnSend
+
+        val radGrp = view.findViewById<RadioGroup>(R.id.opt1)
+
         btnClick.setOnClickListener{
-            SaveInfo(alDialog).execute("")
+            //SaveInfo(alDialog).execute("")
+            val selRad = radGrp.checkedRadioButtonId
+            Log.d("Selected Radio Button", selRad.toString())
+            val rBtn = view.findViewById<RadioButton>(selRad)
+            Toast.makeText(context, rBtn.text, Toast.LENGTH_LONG).show()
         }
 
         return view
     }
+
+//    private fun getRadBtnVal(rg: RadioGroup, v: View){
+//        val selRad = rg.checkedRadioButtonId
+//        val rBtn = v.findViewById(selRad) as RadioButton
+//        Toast.makeText(context, rBtn.editableText, Toast.LENGTH_LONG).show()
+//    }
 
     private class SaveInfo constructor(pBar: AlertDialog): AsyncTask<String, Void, String>() {
         private val pLoading: AlertDialog = pBar
@@ -38,15 +54,19 @@ class FragmentFillUp : Fragment() {
         }
 
         override fun doInBackground(vararg p0: String?): String {
-            for(i in 0..100){
-                println(i)
+            try{
+
+            } catch(e: Exception) {
+
+            } finally {
+
             }
             return ""
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-
+            pLoading.dismiss()
         }
 
     }
