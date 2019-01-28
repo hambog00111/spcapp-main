e83ab61760adbecc9e3e32f88ba45cd3a76d91a0
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-01-28 09:31:34 +0800

    Changes from master branch

--- app/src/main/java/com/example/mis_pc_11/spc_app1/ActivityPictures.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/ActivityPictures.kt
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.mis_pc_11.spc_app1
 
 import android.support.v7.app.AppCompatActivity
@@ -24,3 +25,30 @@ class ActivityPictures : AppCompatActivity() {
          rview.adapter = MyAdapater(place, name, this )
     }
 }
+=======
+package com.example.mis_pc_11.spc_app1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutCompat.VERTICAL
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class ActivityPictures : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pictures)
+
+        val rview = findViewById<View>(R.id.rview) as RecyclerView
+        val place = intArrayOf(R.drawable.slake1,R.drawable.slake2,R.drawable.slake4,R.drawable.slake5,R.drawable.slake6,R.drawable.slake0)
+        val name = arrayOf("Lake1","Lake2","Lake3","Lake4","Lake5","Lake6")
+
+        val lManager = LinearLayoutManager(this, VERTICAL, false)
+        rview.setLayoutManager(lManager)
+
+         rview.adapter = MyAdapater(place, name, this )
+    }
+}
+>>>>>>> Stashed changes
--- app/src/main/java/com/example/mis_pc_11/spc_app1/VideoModels.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/VideoModels.kt
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.mis_pc_11.spc_app1
 
 import java.time.Duration
@@ -15,3 +16,13 @@ class VideoDetail(val name: String, val duration: String, val number: Int, val i
 //[{"name":"Creating a Registration Screen","duration":"21:50","number":1,"imageUrl":"
 // https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/d6938a65-b273-421b-976a-40cf923a17ba
 // _thumbnail","link":"https://www.letsbuildthatapp.com/course_video?id=1022"},
+=======
+package com.example.mis_pc_11.spc_app1
+
+class HomeFeed(val videos: List<Video>)
+
+class Video(val id : Int , val name: String, val link: String, val imageUrl: String,
+            val numberOfViews: Int, val channel: com.example.mis_pc_11.spc_app1.Channel )
+
+class Channel(val name: String, val profileImageUrl: String)
+>>>>>>> Stashed changes
