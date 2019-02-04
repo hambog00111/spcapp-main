4629afa00ccebc7e787242882ab1c4bc654d52fd
MIS-PC_11 <chris.velasco2795@gmail.com>
2019-02-04 14:46:45 +0800

    sda

--- app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentHome.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/FragmentHome.kt
@@ -6,12 +6,16 @@ import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import kotlinx.android.synthetic.*
 
 class FragmentHome : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
    return inflater.inflate(R.layout.fragment_home_layout, null)
+
+
+
 
     }
 
--- app/src/main/java/com/example/mis_pc_11/spc_app1/MainActivity.kt
+++ app/src/main/java/com/example/mis_pc_11/spc_app1/MainActivity.kt
@@ -7,14 +7,18 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(toolbar)
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -22,6 +26,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
         supportFragmentManager.beginTransaction().replace(R.id.frag_container, FragmentHome()).commit()
+
+
     }
 
     override fun onBackPressed() {
@@ -32,6 +38,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId)
+        {
+            R.id.police_menu -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+    }
 
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
